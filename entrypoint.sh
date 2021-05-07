c96bc3a5e139f057a413879fa39a524c8aec3693
Stephan Druskat <mail@sdruskat.net>
2021-05-07 20:15:06 +0200

    Improve feedback echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-echo "Saving $GITHUB_REPOSITORY to Software Heritage"
+echo "Saving https://github.com/$GITHUB_REPOSITORY to Software Heritage"
 result=$(curl -X POST https://archive.softwareheritage.org/api/1/origin/save/git/url/https://github.com/${GITHUB_REPOSITORY}/)
 echo "::set-output name=result::$result"
