4cd6ea77f6a5b58e126584b633f3f2ec8d194f51
Stephan Druskat <mail@sdruskat.net>
2021-05-07 20:25:26 +0200

    Fix link rendering in workflow output

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "Saving https://github.com/$GITHUB_REPOSITORY to Software Heritage"
 result=$(curl -X POST https://archive.softwareheritage.org/api/1/origin/save/git/url/https://github.com/${GITHUB_REPOSITORY}/)
 # In this version, the result var isn't used further
 echo "Save request sent to the Software Heritage API."
-echo "To check for the progress of the save process, go to https://archive.softwareheritage.org/save/#requests."
+echo "To check for the progress of the save process, go to https://archive.softwareheritage.org/save/#requests ."
